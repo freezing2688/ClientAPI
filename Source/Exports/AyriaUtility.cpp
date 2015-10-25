181498e1f831f521ce4705ac5ddf02eeea97fe36
Convery <convery10122@gmail.com>
2015-10-25 22:28:05 +0100

    AyriaUtility rev1

---
+++ Source/Exports/AyriaUtility.cpp
@@ -0,0 +1,60 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2015-10-25
+    Notes:
+        Requested utilities to save space in other modules.
+*/
+
+#pragma warning (disable:4307)
+#include "..\Utility\Crypto\FNV1a.h"
+#include "ClientAPI.h"
+
+// More readable definition of the method.
+#define EXPORTMETHOD(Command, Functor)      \
+    case FNV1a_Compiletime(Command):        \
+        Result = Functor(Variadic);         \
+        break
+
+bool __cdecl AyriaStorage(size_t Command, ...)
+{
+    bool Result = false;
+    va_list Variadic;
+    va_start(Variadic, Command);
+
+    switch (Command)
+    {
+        // Encryption; takes a char *Plaintext, char *Ciphertext, char *IV, char *Key, uint32_t Length.
+        EXPORTMETHOD("AES256Encrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("AES256Decrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("DES3Encrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("DES3Decrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("TEAEncrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("TEADecrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("RC6Encrypt", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("RC6Decrypt", [](va_list Placeholder) { return false; });
+
+        // Keyexchange; takes a char *Request (when peer) and a char *Response.
+        EXPORTMETHOD("HHSStart", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("HHSContinue", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("RSAStart", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("RSAContinue", [](va_list Placeholder) { return false; });
+
+        // Hashing; takes a char *Plaintext, uint32_t PlainLength, char *Result.
+        EXPORTMETHOD("FNV1aHash_64", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("SM3Hash_192", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("SHA3Hash_256", [](va_list Placeholder) { return false; });
+
+        // File readers; takes a char *Data.
+        EXPORTMETHOD("CSVLoad", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("CSVReadNext", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("CSVSkipLine", [](va_list Placeholder) { return false; });
+
+        // Compression; takes a char *Plaintext, char **ModifiedBuffer, uint32_t PlainLength, uint32_t *ModifiedLength.
+        EXPORTMETHOD("LZ4Inflate", [](va_list Placeholder) { return false; });
+        EXPORTMETHOD("LZ4Deflate", [](va_list Placeholder) { return false; });
+    }
+
+    va_end(Variadic);
+    return Result;
+}
