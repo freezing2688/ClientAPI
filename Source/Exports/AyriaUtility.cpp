72330a33d7e25b2c438fe6650f80d6ad94b25cf4
sokiee <sokysrm@gmail.com>
2015-10-26 17:47:02 +0100

    Added documentation for the first implemented API calls. This should be used as a guideline for all future API documentation.

--- Source/Exports/AyriaUtility.cpp
+++ Source/Exports/AyriaUtility.cpp
@@ -19,15 +19,63 @@
 // Implementations.
 namespace APIImplentation
 {
+
+    /**
+     Decompress an lz4 buffer
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, char **OutputBuffer, uint32_t PlaintextLength, int32_t *NumberOfBytesChanged.
+     @return True if the decompression was handled. NOTE: if decompression fails NumberOfBytesChanged will be < 0.
+     */
     extern bool LZ4Inflate(va_list Variadic);
+    /**
+     Compress an lz4 buffer
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, char **OutputBuffer, uint32_t PlaintextLength, int32_t *NumberOfBytesChanged.
+     @return True if the decompression was handled. NOTE: if decompression fails NumberOfBytesChanged will be < 0.
+     */
     extern bool LZ4Deflate(va_list Variadic);
 
+    /**
+     Encrypt a plaintext string using the AES256 encryption algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, char *Ciphertext, char *InitializationVector, char *Encryptionkey, uint32_t Length.
+     @return True if the encryption was handled.
+     */
     extern bool AES256Encrypt(va_list Variadic);
+    /**
+     Decrypt a plaintext string using the AES256 encryption algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, char *Ciphertext, char *InitializationVector, char *Encryptionkey, uint32_t Length.
+     @return True if the decryption was handled.
+     */
     extern bool AES256Decrypt(va_list Variadic);
+    /**
+     Encrypt a plaintext string using the DES3 encryption algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext ,char *Ciphertext,unsigned char *lpKey1,unsigned char *lpKey2,uint32_t Length.
+     @return True if the encryption was successful, false otherwise. If encryption was successful Ciphertext will contain the encrypted ciphertext.
+     */
     extern bool DES3Encrypt(va_list Variadic);
+     /**
+     Decrypt a plaintext string using the DES3 encryption algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext ,unsigned char *Ciphertext,unsigned char *lpKey1,unsigned char *lpKey2,uint32_t Length.
+     @return True if the decryption was successful, false otherwise. If decryption was successful Plaintext will contain the decrypted plaintext.
+     */
     extern bool DES3Decrypt(va_list Variadic);
-
+    /**
+     Hash a plaintext string using the FNV1 hashing algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, uint32_t PlainLength, char *Result.
+     @return True if the hashing was handled. If hashing was successful Result will contain the hash.
+     */
     extern bool FNV1aHash_64(va_list Variadic);
+    /**
+     Hash a plaintext string using the SM3 hashing algorithm
+     
+     @param Variadic a variadic argument containing the parameters to the function. Expected args are char *Plaintext, uint32_t PlainLength, char *Result.
+     @return True if the hashing was handled. If hashing was successful Result will contain the hash.
+     */
     extern bool SM3Hash_192(va_list Variadic);
 };
 
