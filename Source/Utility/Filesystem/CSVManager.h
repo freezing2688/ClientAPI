3c494e2de1df7733581c0a6122a66c72ccb02d2e
Convery <convery10122@gmail.com>
2015-10-26 08:45:40 +0100

    Declared a CSVManager.

---
+++ Source/Utility/Filesystem/CSVManager.h
@@ -0,0 +1,27 @@
+/*
+    Initial author: (https://github.com/)Convery
+    License: LGPL 3.0
+    Started: 2015-10-26
+    Notes:
+        CSV reading and writing.
+*/
+
+#pragma once
+#include <string>
+#include <vector>
+
+class CSVManager
+{
+    FILE *Filehandle;
+    std::vector<std::vector<std::string>> Buffer;
+
+public:
+    bool BeginRead(const char *Filename);
+    bool ReadNext(std::string &NextItem);
+    bool SkipLine();
+
+    bool Insert(int32_t Row, int32_t Col, std::string Entry);
+    bool WriteFile(const char *Filename);
+    bool CloseFile();
+};
+
