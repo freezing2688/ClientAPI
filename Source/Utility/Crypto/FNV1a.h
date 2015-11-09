20fe9b07d59dfc25fac5726597c384799e0b16a3
Convery <Convery10122@gmail.com>
2015-11-09 10:33:27 +0100

    Bugfix and FNV expansion.

--- Source/Utility/Crypto/FNV1a.h
+++ Source/Utility/Crypto/FNV1a.h
@@ -12,6 +12,24 @@
 constexpr size_t FNV1_Prime = (sizeof(void *) == 8 ? 1099511628211u : 16777619u);
 constexpr size_t FNV1_Offset = (sizeof(void *) == 8 ? 14695981039346656037u : 2166136261u);
 
+// FNV1 implementation.
+constexpr size_t FNV1_Compiletime(const char *String, size_t LastValue = FNV1_Offset)
+{
+    return *String ? FNV1_Compiletime(String + 1, (*String * FNV1_Prime) ^ LastValue) : LastValue;
+};
+inline size_t FNV1_Runtime(char *Data, size_t Length)
+{
+    size_t Hash = FNV1_Offset;
+
+    for (size_t i = 0; i < Length; ++i)
+    {
+        Hash *= FNV1_Prime;
+        Hash ^= Data[i];
+    }
+
+    return Hash;
+};
+
 // FNV1a implementation.
 constexpr size_t FNV1a_Compiletime(const char *String, size_t LastValue = FNV1_Offset)
 {
